c666ce79063fc717d0279393a39bd48adc496294
RVParvanov22 <132457810+RVParvanov22@users.noreply.github.com>
2024-11-01 17:46:52 +0200

    Add login system

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,9 +4,7 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +15,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_loginButton_clicked();  // Слот за бутона за логин
+
 private:
     Ui::MainWindow *ui;
+
+    // Функция за проверка на потребителско име и парола
+    bool verifyCredentials(const QString &username, const QString &password);
 };
+
 #endif // MAINWINDOW_H
