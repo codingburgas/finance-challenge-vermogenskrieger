c59cc27edd94969ce8281d887ee31288b14ac0d2
Anton Teodorov Kabakov <ATKabakov22@codingburgas.bg>
2024-11-03 18:08:28 +0200

    Add logo and title to the taskbar

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include <QSystemTrayIcon>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+
+    #ifdef Q_OS_WINDOWS
+        QIcon appIcon("C:/Users/anton/source/repos/finance-challenge-vermogenskrieger/src/images/fullLogo.ico");
+    #else
+       QIcon appIcon("images/fullLogo.ico");
+    #endif
+
+    a.setWindowIcon(appIcon);
+    QSystemTrayIcon trayIcon;
+    trayIcon.setIcon(appIcon);
+
+    w.setWindowTitle("Vermögenskrieger");
+
+    trayIcon.show();
+
     w.show();
     return a.exec();
 }
